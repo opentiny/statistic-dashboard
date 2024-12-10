25d35c92950ce8fec4a928be79480387bd3343fe
Gimmy <975402925@qq.com>
2024-12-10 16:54:35 +0800

    feat: add table data

--- packages/dashboard/src/App.vue
+++ packages/dashboard/src/App.vue
@@ -1,30 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DataStat from './components/DataStat.vue'
+import ConfigProvider from '@opentiny/vue-config-provider'
+import designSmbConfig from '@opentiny/vue-design-smb'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <config-provider :design="designSmbConfig">
+    <DataStat msg="Vite + Vue" />
+  </config-provider>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- packages/dashboard/vite.config.js
+++ packages/dashboard/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd() + '/env', '')
+  return {
+    plugins: [vue(), vueJsx()],
+    envDir: './env',
+    base: env.VITE_BASE_URL,
+    resolve: {
+    }
+  }
 })
